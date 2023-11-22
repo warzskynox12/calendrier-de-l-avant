2f271b89d22c10ea749e0d8408e4efdacd9e2ff0
lucas pasnon <lucas.pasnon@outlook.fr>
2023-11-22 21:31:24 +0100

    jour1

---
+++ index.js
@@ -0,0 +1,24 @@
+function ouvrirPorte(jour) {
+  var dateActuelle = new Date();
+  var anneeActuelle = dateActuelle.getFullYear();
+  var moisActuel = dateActuelle.getMonth() + 1; // getMonth() retourne les mois de 0 à 11
+  var jourActuel = dateActuelle.getDate();
+
+  if (moisActuel === 12 && anneeActuelle === 2023) {
+    if (jourActuel >= jour) {
+      etatCalendrier[jour] = true;
+      document.getElementById("day" + jour).classList.add("opened");
+      window.location.href = "./" + jour + ".html";
+    } else {
+      alert(
+        "c'est pas bien de tricher, mais vous devez attendre jusqu'au jour " +
+          jour +
+          " pour ouvrir cette porte."
+      );
+    }
+  } else {
+    alert(
+      "Cette porte ne peut être ouverte que pendant le mois de décembre de l'année 2023."
+    );
+  }
+}
