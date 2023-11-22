a484a778bcb34c41a7c967abf28ff54d12f22d9e
lucas pasnon <lucas.pasnon@outlook.fr>
2023-11-22 21:47:26 +0100

    Y

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function ouvrirPorte(jour) {
   var moisActuel = dateActuelle.getMonth(); // getMonth() retourne les mois de 0 à 11
   var jourActuel = dateActuelle.getDate();
 
-  if (moisActuel === 11 && anneeActuelle === 2023) {
+  if (moisActuel === 10 && anneeActuelle === 2023) {
     if (jourActuel >= jour) {
       document.getElementById("day" + jour).classList.add("opened");
       window.location.href = "./" + jour + ".html";
