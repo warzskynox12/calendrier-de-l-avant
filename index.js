39b9ae61f01b5b585e11d1ccfbf96fc60a7ec947
pasnon lucas <107144928+warzskynox12@users.noreply.github.com>
2023-11-26 17:52:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function ouvrirPorte(jour) {
   var moisActuel = dateActuelle.getMonth() + 1;
   var jourActuel = dateActuelle.getDate();
 
-  if (moisActuel >= 11) {
+  if (moisActuel >= 12) {
     if (jourActuel >= jour) {
       document.getElementById("day" + jour).classList.add("opened");
       window.location.href = "./html/1 copy " + jour + ".html";
